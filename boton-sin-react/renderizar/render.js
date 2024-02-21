c1ad7f5cc26d1d96c104ad12fdabe77792e1fb95
DeibyAA <deibyarroyave4@gmail.com>
2024-02-21 18:50:46 -0500

    b

---
+++ boton-sin-react/renderizar/render.js
@@ -0,0 +1,12 @@
+import React from "https://esm.sh/react@18.2.0"
+import ReactDOM from "https://esm.sh/react-dom@18.2.0/client"
+
+const appDomElement = document.getElementById('app')
+
+const root = ReactDOM.createRoot(appDomElement)
+
+const button = React.createElement('button', { "data-id": 123 }, 'Button 1')
+const button2 = React.createElement('button', { "data-id": 456 }, 'Button 2')
+const button3 = React.createElement('button', { "data-id": 678 }, 'Button 3')
+
+root.render(button)
