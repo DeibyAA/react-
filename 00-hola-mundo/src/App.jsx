9599f2d77e89e5af53f67f84ac3a1954c1930ebc
DeibyAA <deibyarroyave4@gmail.com>
2024-02-27 18:35:54 -0500

    final 00-hola-mundo

--- 00-hola-mundo/src/App.jsx
+++ 00-hola-mundo/src/App.jsx
@@ -2,19 +2,39 @@ import { useState } from 'react'
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
+const users = [
+    {
+        userName: 'midudev',
+        name: 'Ginés Miñano Bernabeu',
+        isFollowing: true
+    },
+    {
+        userName: 'pheralb',
+        name: 'Pablon H.',
+        isFollowing: false
+    },
+    {
+        userName: 'TMChein',
+        name: 'Tomas',
+        isFollowing: true
+    }
+]
 export function App () {
-    const [name, setName] = useState('midudev')
+    const [state, setState] = useState(0)
     return (
         <section className='App'>
-            <TwitterFollowCard userName={name} >Ginés Miñano Bernabéu
-            </TwitterFollowCard>
-
-            <TwitterFollowCard userName="pheralb">Pablo Hernandez
-            </TwitterFollowCard>
-
-            <button onClick={() => setName('pedromichel')}>
-            Cambio nombre
-            </button>
+        {
+            users.map(({ userName, name, isFollowing }) => (
+                <TwitterFollowCard
+                    key={userName}
+                    userName={userName}
+                    initialIsFollowing={isFollowing}
+                    >
+                    {name}
+                </TwitterFollowCard>
+            ))
+        }
+            <button onClick={() => setState(state + 1)}>count</button>
         </section>
     )
 }
