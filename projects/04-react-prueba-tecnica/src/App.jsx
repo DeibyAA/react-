b3c340a9287a48d1a4b0ec968c610903ce5eb6e8
DeibyAA <deibyarroyave4@gmail.com>
2024-03-07 18:15:38 -0500

    useState y useEffect

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,16 +1,21 @@
 import { useEffect, useState } from 'react'
 
 const CAT_ENDPOINT_RAMDOM_FACT = 'https://catfact.ninja/fact'
-//const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&json=true`
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&json=true`
 
-function App() {
+export function App () {
   const [fact, setFact] = useState()
 
-  useEffect (() => {
+  useEffect(() => {
     fetch(CAT_ENDPOINT_RAMDOM_FACT)
-    .then(res => res.json())
-    .then(data => setFact(data.fact))
-  }, []) 
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+
+        const firstWord = fact.split(' ')[0]
+      })
+  }, [])
 
   return (
     <main>
